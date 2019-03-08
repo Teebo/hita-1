90f03916783e52cf2ed0a7a102cc94c6c7b97260
Teebo <ngubanethabo.ambrose@gmail.com>
2019-03-08 16:50:09 +0200

    WIP

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { LandingFixService } from '../shared/services/landing-fix.service';
 import { UIHelperService } from '../landing-page/ui-helper.service';
 import { Socket } from 'ngx-socket-io';
+import anime from 'animejs';
 
 @Component({
   selector: 'app-demo',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit, OnDestroy {
+export class LandingPageComponent implements OnInit, OnDestroy, AfterViewInit {
   myEndVal = undefined;
   tweets = [];
+  animatedText = "Some animation paragraphp 1";
 
   constructor(
     private fix: LandingFixService,
@@ -19,16 +21,16 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    this.socket.on('connect', () => {
-      console.log('Connected');
-      this.socket.emit('getTweets');
-    });
+    // this.socket.on('connect', () => {
+    //   console.log('Connected');
+    //   this.socket.emit('getTweets');
+    // });
 
-    this.socket.on('tweets', d => {
-      console.log(d);
-      this.tweets.push(d);
-      console.log(this.tweets);
-    });
+    // this.socket.on('tweets', d => {
+    //   console.log(d);
+    //   this.tweets.push(d);
+    //   console.log(this.tweets);
+    // });
 
     this.uiHelperService
       .getObservableCanStartCountUpValue()
@@ -43,5 +45,18 @@ export class LandingPageComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.socket.removeListener('tweets');
     this.fix.removeFix();
+  }
+
+  ngAfterViewInit() {
+    anime({
+      targets: 'div.heading1',
+      opacity: 10,
+      keyframes: [
+        {translateX: 300, duration: 1800},
+        {translateX: 5500, duration: 1500},
+      ],
+      delay: 1500,
+      loop: true
+    });
   }
 }
